f130a2cab848a9868e9b80898fb65b1f0262c43b
krebs1 <fantom.candle@mail.ru>
2024-02-17 22:24:21 +0400

    refactor: move app.module.ts to src

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
-import { ConfigService } from '@nestjs/config'
-import { Logger } from '@nestjs/common'
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService:ConfigService = app.get(ConfigService);
+  const configService: ConfigService = app.get(ConfigService);
   const serverConfig = configService.get('server');
-  const {host, port} = serverConfig;
+  const { host, port } = serverConfig;
 
   await app.listen(port, host, () => {
     Logger.log(`Server ready on http://${host}:${port}`, 'Application');
