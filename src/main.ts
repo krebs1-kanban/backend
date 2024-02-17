f7720300dc2b9d2e3339bc889ce148275c8707a3
krebs1 <fantom.candle@mail.ru>
2024-02-17 16:51:44 +0400

    fix: add config service to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { ConfigService } from '@nestjs/config'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService:ConfigService = app.get(ConfigService);
+  const serverConfig = configService.get('server');
+  const {host, port} = serverConfig;
+
+  await app.listen(port, host, () => {
+    Logger.log(`Server ready on http://${host}:${port}`, 'Application');
+  });
 }
 bootstrap();
