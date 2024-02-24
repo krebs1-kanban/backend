dfd70e8f4fc288868b1cc04c0fca62298b91cbc6
krebs1 <fantom.candle@mail.ru>
2024-02-25 01:58:47 +0400

    feat: add cors, cookie parser, global prefix to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
 
   const configService: ConfigService = app.get(ConfigService);
   const serverConfig = configService.get('server');
